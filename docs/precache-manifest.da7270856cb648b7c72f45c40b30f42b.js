ac097aa6bb959dddea8db94a0c4c16b31a1fa028
Vitor Hugo Pacheco <1160741@isep.ipp.pt>
2019-02-05 12:38:40 +0000

    First Commit

---
+++ docs/precache-manifest.da7270856cb648b7c72f45c40b30f42b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "4ea2bbf7d675bef2d8c7",
+    "url": "/react-tutorial/static/js/main.4ea2bbf7.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "4ea2bbf7d675bef2d8c7",
+    "url": "/react-tutorial/static/css/main.995c644e.chunk.css"
+  },
+  {
+    "revision": "90b0608a304e287587d1acba15f9232c",
+    "url": "/react-tutorial/index.html"
+  }
+];
